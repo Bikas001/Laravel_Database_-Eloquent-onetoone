b2f75699b22bf72aa221b8742fc0a9d7cb0cabbb
Bikas chaudhary tharu <vcbikash123@gmail.com>
2020-12-17 15:58:11 +0545

    One To One Relationship in Eloquent

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+use App\Models\User;
+use App\Models\Address;
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/insert', function (){
+
+    $user=User::findOrFail(1);
+
+    $address=new Address(['name'=>'kathmandu nepal']);
+
+    $user->address()->save($address);
+});
+
+Route::get('/update', function (){
+    $address=Address::whereUserId(1)->first();
+    $address->name ="Updated Chitwan";
+    $address->save();
+});
+
+Route::get('/read', function (){
+    $user=User::findOrFail(1);
+    echo $user-> address ->name;
+});
+
+Route::get('/delete', function (){
+    $user=User::findOrFail(1);
+    $user->address->delete();
+});
